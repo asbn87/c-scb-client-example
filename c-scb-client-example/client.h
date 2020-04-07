2a99760845661e0ca1fe153210058b31eebd4434
Andreas Bengtsson <asbn87@gmail.com>
2020-04-07 19:22:55 +0200

    minor changes

--- c-scb-client-example/client.h
+++ c-scb-client-example/client.h
@@ -7,23 +7,23 @@
 
 #pragma comment(lib, "Winhttp.lib")
 
-#define DEFAULT_BUFLEN 40960												// Default buflength of page, increase if you need.
+#define DEFAULT_BUFLEN 40960                                                // Default buflength of page, increase if you need.
 
-char in_buf[DEFAULT_BUFLEN];												// In buf
-char out_buf[DEFAULT_BUFLEN];												// Out buf
+char in_buf[DEFAULT_BUFLEN];                                                // In buf
+char out_buf[DEFAULT_BUFLEN];                                               // Out buf
 
-BOOL res;																	// Return result 												
+BOOL res;                                                                   // Return result 												
 HINTERNET 
-	hSession,																// Session handler
-	hConnect,																// Connection handler
-	hRequest;																// Request handler
+	hSession,                                                               // Session handler
+	hConnect,                                                               // Connection handler
+	hRequest;                                                               // Request handler
 
-void winhttp_init();														// Use WinHttpOpen to obtain a session handle.
-void winhttp_connect(unsigned short* host);									// Specify an HTTP server.
-void winhttp_open_request(unsigned short* cmd, unsigned short* path);		// Create an HTTP request handle.
-void winhttp_send_request();												// Send a request.
-void winhttp_end_request();													// End the request.
-void winhttp_read_data();													// Read data until end
-void winhttp_print_data();													// Print in_buf
-void report_error();														// Report any errors.
-void winhttp_close();														// Close any open handles.
+void winhttp_init();                                                        // Use WinHttpOpen to obtain a session handle.
+void winhttp_connect(unsigned short* host);                                 // Specify an HTTP server.
+void winhttp_open_request(unsigned short* cmd, unsigned short* path);       // Create an HTTP request handle.
+void winhttp_send_request();                                                // Send a request.
+void winhttp_end_request();                                                 // End the request.
+void winhttp_read_data();                                                   // Read data until end
+void winhttp_print_data();                                                  // Print in_buf
+void report_error();                                                        // Report any errors.
+void winhttp_close();                                                       // Close any open handles.
