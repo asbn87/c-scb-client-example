5564d0b8dbaf6b4696d1711c896299604370b92c
Andreas Bengtsson <asbn87@gmail.com>
2020-04-09 11:52:42 +0200

    Changed tab settings

--- c-scb-client-example/client.h
+++ c-scb-client-example/client.h
@@ -7,24 +7,23 @@
 
 #pragma comment(lib, "Winhttp.lib")
 
-#define DEFAULT_BUFLEN 40960												// Default buflength of page, increase if you need.
+#define DEFAULT_BUFLEN 40960                                            // Default buflength of page, increase if you need.
 
-char in_buf[DEFAULT_BUFLEN];												// In buf
-char out_buf[DEFAULT_BUFLEN];												// Out buf
+char in_buf[DEFAULT_BUFLEN];                                            // In buf
+char out_buf[DEFAULT_BUFLEN];                                           // Out buf
 
-BOOL res;																	// Return result
-HINTERNET 
-	hSession,																// Session handler
-	hConnect,																// Connection handler
-	hRequest;																// Request handler
+BOOL res;                                                               // Return result
+HINTERNET
+    hSession,                                                           // Session handler
+    hConnect,                                                           // Connection handler
+    hRequest;                                                           // Request handler
 
-void winhttp_init();														// Use WinHttpOpen to obtain a session handle.
-void winhttp_connect(unsigned short* host);									// Specify an HTTP server.
-void winhttp_open_request(unsigned short* cmd, unsigned short* path);		// Create an HTTP request handle.
-int winhttp_send_request();													// Send a request.
-int winhttp_end_request();													// End the request.
-int winhttp_read_data();													// Read data until end
-void winhttp_print_data();													// Print in_buf
-void report_error();														// Report any errors.
-void winhttp_close();														// Close any open handles.
-
+void winhttp_init();                                                    // Use WinHttpOpen to obtain a session handle.
+void winhttp_connect(unsigned short* host);                             // Specify an HTTP server.
+void winhttp_open_request(unsigned short* cmd, unsigned short* path);   // Create an HTTP request handle.
+int winhttp_send_request();                                             // Send a request.
+int winhttp_end_request();                                              // End the request.
+int winhttp_read_data();                                                // Read data until end
+void winhttp_print_data();                                              // Print in_buf
+void report_error();                                                    // Report any errors.
+void winhttp_close();                                                   // Close any open handles.
--- c-scb-client-example/main.c
+++ c-scb-client-example/main.c
@@ -3,27 +3,26 @@
 
 int main() {
 
-	// Connecting to the open api at the Swedish Central Agency for statistics
-	unsigned short* host = L"api.scb.se";
+    // Connecting to the open api at the Swedish Central Agency for statistics
+    unsigned short* host = L"api.scb.se";
 
-	// Table with names of newborns with at least 10 namings since 1998
-	unsigned short* path = L"/OV0104/v1/doris/sv/ssd/START/BE/BE0001/BE0001D/BE0001T04Ar";
+    // Table with names of newborns with at least 10 namings since 1998
+    unsigned short* path = L"/OV0104/v1/doris/sv/ssd/START/BE/BE0001/BE0001D/BE0001T04Ar";
 
-	unsigned short* cmd = L"GET";
+    unsigned short* cmd = L"GET";
 
-	winhttp_init();
-	winhttp_connect(host);
-	winhttp_open_request(cmd, path);
-	winhttp_send_request();
-	winhttp_end_request();
-	winhttp_read_data();
+    winhttp_init();
+    winhttp_connect(host);
+    winhttp_open_request(cmd, path);
+    winhttp_send_request();
+    winhttp_end_request();
+    winhttp_read_data();
 
-	winhttp_print_data();
+    winhttp_print_data();
 
-	report_error();
-	winhttp_close();
+    report_error();
+    winhttp_close();
 
-	getchar();
-
-	return 0;
+    getchar();
+    return 0;
 }
